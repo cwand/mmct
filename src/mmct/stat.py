26139c742f55de5feff2b546e368a2b20e2228ee
Christian Walther Andersen <cvvand@gmail.com>
2021-04-23 21:24:25 +0200

    Removed util.py

--- src/mmct/stat.py
+++ src/mmct/stat.py
@@ -1,8 +1,6 @@
 import math
 import numpy as np
 
-import mmct.util as util
-
 # Computes the log-likelihood ratio for a set of multinomially distributed
 # observations x, compared to a reference (expectation under
 # the null-hypothesis). The total number of observations in the two arrays
@@ -14,12 +12,11 @@ def multinomialLLR(x, ref):
   if x.size != ref.size:
     raise ValueError('Input arrays must have the same number of elements')
 
-  null_prob = util.normL1(ref)
   n = x.sum()
 
   llr = 0.0
   for i in range(0,x.size):
     if (x[i] != 0):
-      llr += x[i] * math.log(n*null_prob[i]/x[i])
+      llr += x[i] * math.log(n*ref[i]/x[i])
 
   return llr
