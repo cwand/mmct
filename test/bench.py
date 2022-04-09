a4ee249bef5ed6d06649d6a09446199a0c208895
Chris W. Andersen <cvvand@gmail.com>
2022-04-10 00:09:27 +0200

    Cleanup for tox, all clear!

--- test/bench.py
+++ test/bench.py
@@ -2,22 +2,14 @@ import mmct
 import time
 import numpy as np
 
-# Hypothsised probability
-
-p = np.array([1/36, 2/36, 3/36, 4/36, 5/36, 6/36, 5/36, 4/36, 3/36, 2/36, 1/36])
-
-
-# Observations
-
-x = np.array([0, 0, 2, 4, 5, 2, 3, 1, 0, 1, 2])
-
-
 t0 = time.time()
 
 tx = mmct.tester()
 tx.n_samples = 30000
 
-p1 = tx.do_test(x, p)
+p1 = tx.do_test(
+	np.array([0, 0, 2, 4, 5, 2, 3, 1, 0, 1, 2]),
+	np.array([1 / 36, 2 / 36, 3 / 36, 4 / 36, 5 / 36, 6 / 36, 5 / 36, 4 / 36, 3 / 36, 2 / 36, 1 / 36]))
 print(f'Calculated p-value: {p1:.3f}')
 
 t1 = time.time()
--- test/test_core.py
+++ test/test_core.py
@@ -11,8 +11,7 @@ class TestTester(unittest.TestCase):
 		t.n_samples = 200
 		t.statistics = np.zeros(80)
 
-		x = np.array([3, 4, 5, 6])
-		t.do_test(x, np.array([0.2, 0.25, 0.3, 0.25]))
+		t.do_test(np.array([3, 4, 5, 6]), np.array([0.2, 0.25, 0.3, 0.25]))
 
 		self.assertEqual(t.statistics.size, 200)
 
@@ -20,9 +19,7 @@ class TestTester(unittest.TestCase):
 		t = mmct.tester()
 		t.n_samples = 4
 
-		x = np.array([3, 4, 5, 6])
-
-		t.do_test(x, np.array([0.2, 0.25, 0.3, 0.25]))
+		t.do_test(np.array([3, 4, 5, 6]), np.array([0.2, 0.25, 0.3, 0.25]))
 
 		# Set artificial and impossible statistics. If generate_samples is run, these
 		# values will be overwritten, since they cannot occur mathematically
@@ -30,9 +27,7 @@ class TestTester(unittest.TestCase):
 
 		t.fix = True
 
-		y = np.array([6, 5, 4, 3])
-
-		t.do_test(y, np.array([0.2, 0.25, 0.3, 0.25]))
+		t.do_test(np.array([6, 5, 4, 3]), np.array([0.2, 0.25, 0.3, 0.25]))
 
 		self.assertEqual(t.statistics.size, 4)
 		self.assertEqual(t.statistics[0], 10)
@@ -44,8 +39,8 @@ class TestTester(unittest.TestCase):
 		t = mmct.tester()
 		t.n_samples = 5
 
-		x = np.array([1, 3, 1, 3])  # Prob = 0.0189
-		t.do_test(x, np.array([0.05, 0.6, 0.1, 0.25]))
+		# np.array([1, 3, 1, 3])  Prob = 0.0189
+		t.do_test(np.array([1, 3, 1, 3]), np.array([0.05, 0.6, 0.1, 0.25]))
 
 		# null prob: [0.05,0.6,0.1,0.25]
 		# Sample 0: [0,4,2,2]			Prob = 0.03402
@@ -56,25 +51,24 @@ class TestTester(unittest.TestCase):
 		t.statistics = np.array([0.03402, 0.00653184, 0.0189, 0.01306368, 0.0000252])
 		t.fix = True
 
-		p = t.do_test(x, np.array([0.05, 0.6, 0.1, 0.25]))
+		p = t.do_test(np.array([1, 3, 1, 3]), np.array([0.05, 0.6, 0.1, 0.25]))
 
 		self.assertEqual(p, 0.8)
 
-		x = np.array([8, 0, 0, 0])  # Prob = 3.90625 × 10^-11
-		p = t.do_test(x, np.array([0.05, 0.6, 0.1, 0.25]))
+		# np.array([8, 0, 0, 0])  Prob = 3.90625 × 10^-11
+		p = t.do_test(np.array([8, 0, 0, 0]), np.array([0.05, 0.6, 0.1, 0.25]))
 
 		self.assertEqual(p, 0.0)
 
-		x = np.array([1, 2, 2, 3])  # Prob = 0.004725
-		p = t.do_test(x, np.array([0.05, 0.6, 0.1, 0.25]))
+		# np.array([1, 2, 2, 3])  Prob = 0.004725
+		p = t.do_test(np.array([1, 2, 2, 3]), np.array([0.05, 0.6, 0.1, 0.25]))
 
 		self.assertEqual(p, 0.2)
 
 	def test_tester_do_test_error_x_probs_not_same_dim(self):
 		t = mmct.tester()
-		x = np.array([3, 4, 5])
-		p = np.array([0.3, 0.6, 0.05, 0.05])
-		self.assertRaises(ValueError, t.do_test, x, p)
+		self.assertRaises(ValueError, t.do_test,
+			np.array([3, 4, 5]), np.array([0.3, 0.6, 0.05, 0.05]))
 
 
 class TestMTTester(unittest.TestCase):
@@ -84,7 +78,6 @@ class TestMTTester(unittest.TestCase):
 		t.n_samples = 200
 		t.statistics = np.zeros(80)
 
-		x = np.array([3, 4, 5, 6])
-		t.do_test(x, np.array([0.2, 0.25, 0.3, 0.25]))
+		t.do_test(np.array([3, 4, 5, 6]), np.array([0.2, 0.25, 0.3, 0.25]))
 
 		self.assertEqual(t.statistics.size, 200)
