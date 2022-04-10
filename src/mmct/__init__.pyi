0c69e47ab2b20c5268190110735a76e345c283de
Chris W. Andersen <cvvand@gmail.com>
2022-04-10 23:19:27 +0200

    mt_tester broken, otherwise fine. Writing doc in core.py

--- src/mmct/__init__.pyi
+++ src/mmct/__init__.pyi
@@ -2,32 +2,25 @@ import numpy as np
 import numpy.typing as npt
 from multiprocessing import Pool
 
-# Main class used for performing multinomial tests
 
 class tester:
 
-	n_samples: int  # The number of Monte Carlo samples to generate
-	statistics: npt.NDArray[np.float_]  # Test statistics of random samples
+	n_samples: int
+	statistics: npt.NDArray[np.float_]
 
-	# The test statistics can be fixed by setting fix to True. This prevents
-	# rerunning the Monte Carlo sampling when running a new test. This can
-	# be useful when testing or to save time (bias danger!)
 	fix: bool
 
 	def generate_sample_stat(self, index: int) -> float: ...
 
-	def mc_runs(self) -> None: ...
+	def mc_runs(self, n_obs: int, probs: npt.NDArray[np.float_]) -> None: ...
 
 	def do_test(self,
 		x: npt.NDArray[np.int_], probs: npt.NDArray[np.float_]) -> float: ...
 
 
-
-# Derived class from tester that uses multithreading to speed up the monte carlo
-# sampling
-
 class mt_tester(tester):
 
-	# Number of threads to use. If None, the system default is used
-	# (typically the number of logical processors)
 	threads: int
+
+
+__all__: list[str]
--- test/bench.py
+++ test/bench.py
@@ -5,15 +5,17 @@ import numpy as np
 t0 = time.time()
 
 tx = mmct.tester()
-tx.n_samples = 30000
+tx.n_samples = 80000
 
-p1 = tx.do_test(
-	np.array([0, 0, 2, 4, 5, 2, 3, 1, 0, 1, 2]),
-	np.array([1 / 36, 2 / 36, 3 / 36, 4 / 36, 5 / 36, 6 / 36, 5 / 36, 4 / 36, 3 / 36, 2 / 36, 1 / 36]))
-print(f'Calculated p-value: {p1:.3f}')
+#p1 = tx.do_test(
+#	np.array([0, 0, 2, 4, 5, 2, 3, 1, 0, 1, 2]),
+#	np.array([1 / 36, 2 / 36, 3 / 36, 4 / 36, 5 / 36, 6 / 36, 5 / 36, 4 / 36, 3 / 36, 2 / 36, 1 / 36]))
+
+p1 = tx.do_test(np.array([13,2,9,11,8,7]),np.array([1/6,1/6,1/6,1/6,1/6,1/6]))
 
 t1 = time.time()
 
+print(f'Calculated p-value: {p1:.3f}')
 
 print('')
 print("Time: {:.4f}s".format(t1 - t0))
